4dc199ae7ee7d4897da988df7b5a3ce85fefc681
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-06-18 15:09:31 +0900

    テストプロジェクトの追加と意図的な失敗テスト

---
+++ CS_Actions_WebApp.Tests/Services/CalcServiceTests.cs
@@ -0,0 +1,32 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CS_Actions_WebApp.Services;
+namespace CS_Actions_WebApp.Tests.Services;
+/// <summary>
+/// 計算機能を提供するサービスクラスの単体テストドライバ
+/// </summary>
+[TestClass]
+public class CalcServiceTests
+{
+    /// <summary>
+    /// 加算処理のテスト
+    /// </summary>   
+    [TestMethod]
+    public void Execute_Addition_ReturnsCorrectValue()
+    {
+        var service = new CalcService();
+        var result = service.Execute(10, 5, "+");
+        // テストを失敗させる
+        Assert.AreEqual(10, result);
+    }
+
+    /// <summary>
+    /// ゼロ除算処理のテスト
+    /// </summary>
+    [TestMethod]
+    [ExpectedException(typeof(DivideByZeroException))]
+    public void Execute_DivideByZero_ThrowsException()
+    {
+        var service = new CalcService();
+        service.Execute(10, 0, "/");
+    }      
+}
---
+++ CS_Actions_WebApp.Tests/UnitTest1.cs
@@ -0,0 +1,10 @@
+namespace CS_Actions_WebApp.Tests;
+
+[TestClass]
+public class UnitTest1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+    }
+}
