2bebcfea351e63bff192c481d1b89ba3dc72543c
toukaon66 <tou.kaon.howsplan@gmail.com>
2026-06-18 16:40:27 +0900

    Testcontainersを用いたDB連動テストの追加

---
+++ CS_Actions_WebApp/Infrastructures/Repository/ProductRepository.cs
@@ -0,0 +1,25 @@
+using Microsoft.EntityFrameworkCore;
+using CS_Actions_WebApp.Infrastructures.Entity;
+using CS_Actions_WebApp.Infrastructures.Context;
+namespace CS_Actions_WebApp.Infrastructures.Repository;
+/// <summary>
+/// 商品情報のデータアクセスを担うリポジトリクラス
+/// </summary>
+public class ProductRepository
+{
+    private readonly AppDbContext _context;
+
+    public ProductRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    // <summary>
+    /// 全ての商品をID順に取得する
+    /// virtualを付与しているのは、Moqでこのメソッドを上書きできるようにするため
+    /// </summary>
+    public virtual async Task<List<Product>> SelectAllAsync()
+    {
+        return await _context.Products.OrderBy(p => p.Id).ToListAsync();
+    }
+}
